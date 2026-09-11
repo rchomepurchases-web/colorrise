7d6f6202865f0253365bd55f48dacc2dce6b6b01
rchomepurchases-web <rchomepurchases@gmail.com>
2026-09-10 17:41:47 -0700

    Load mobile homepage fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "./mobile-fixes.css";
 import { businessName, siteUrl } from "./seo";
 import ConversionTracking from "./ConversionTracking";
 
