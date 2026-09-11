5a6cd826d8b291f70105120e1b66644f4a187985
rchomepurchases-web <rchomepurchases@gmail.com>
2026-09-10 17:57:29 -0700

    Load refined mobile hero styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./mobile-fixes.css";
+import "./mobile-hero-v2.css";
 import { businessName, siteUrl } from "./seo";
 import ConversionTracking from "./ConversionTracking";
 
